bd8dcc6662088c4afc647761dd545ecab9bcf07a
matson3 <matson.kevin@yahoo.com>
2018-08-24 16:57:06 -0400

    Added specialized classes for automatic run-time unit checking. Also makes required classes clearer.

--- Physics/HW1.cpp
+++ Physics/HW1.cpp
@@ -10,11 +10,23 @@ int main() {
 
 	cout << "HW01" << endl << endl;
 
-	Quantity r = Quantity(0.53e-10, "m");
+	DistanceM r = DistanceM(0.53e-10);
 	cout << "Model: hydrogen atom; electron moves around proton nucleus in circular orbit of radius " << r << endl << endl;
 
 	cout << "Question 1" << endl;
-	cout << "Determine force between electron & proton.\n " << elecForce(e, -e, r) << endl;
+	cout << " A. Determine force between electron & proton." << endl;
+	ForceM electricForce = elecForce(e, -e, r);
+	cout << "  " << electricForce << endl << endl;
+
+	cout << " B. Determine the radial acceleration of the electron." << endl;
+	Quantity radialAcceleration = electricForce / electronMass;
+	radialAcceleration.unit = Unit("m / s^2"); // TODO: Automatically convert units
+	cout << "  " << radialAcceleration << endl << endl;
+
+	cout << " C. Determine the speed of the electron." << endl;
+	VelocityM rv = circularVelocity(r, radialAcceleration);
+	cout << "  " << rv << endl << endl;
+
 
 	return 0;
 }
--- Physics/Physics.h
+++ Physics/Physics.h
@@ -41,7 +41,10 @@ namespace phys {
 		bool operator == (const Unit& u) const;
 		bool operator != (const Unit& u) const { return !(*this == u); }
 
-		friend std::ostream& operator << (std::ostream& o, const Unit& u);
+		inline friend std::ostream& operator << (std::ostream& o, const Unit& u) { return o << u.toString(); }
+
+		Unit sqrt() const;
+		std::string toString() const;
 	};
 
 	class Quantity {
@@ -53,7 +56,9 @@ namespace phys {
 		Quantity(double value) : quantity(value) {}
 		Quantity() : quantity(0), unit("") {}
 
-		Quantity magnitude() { return Quantity((quantity < 0) ? -quantity : quantity, unit); }
+		Quantity magnitude() const { return Quantity((quantity < 0) ? -quantity : quantity, unit); }
+		Quantity sqrt() const { return Quantity(::sqrt(quantity), unit.sqrt()); }
+		std::string toString() const;
 
 		friend Quantity operator * (double d, const Quantity& q) { return Quantity(d) * q; }
 		inline Quantity operator * (const Quantity& q) const { return Quantity(quantity * q.quantity, unit * q.unit); }
@@ -68,7 +73,7 @@ namespace phys {
 		Quantity operator - (const Quantity& q) const { if (unit != q.unit) throw Exception("Units are not the same"); return Quantity(quantity - q.quantity, q.unit); }
 		Quantity operator - () const { return Quantity(-quantity, unit); }
 
-		friend std::ostream& operator << (std::ostream& o, const Quantity& q) { return o << q.quantity << ' ' << q.unit; }
+		inline friend std::ostream& operator << (std::ostream& o, const Quantity& q) { return o << q.toString(); }
 	};
 
 	class Vector {
@@ -80,7 +85,7 @@ namespace phys {
 
 		inline Quantity magnitude() const { return Quantity(sqrt(x*x + y * y + z * z), unit); }
 
-		inline Vector unitV() const { return *this / magnitude();	}
+		inline Vector unitV() const { return *this / magnitude(); }
 
 		inline Quantity operator * (const Vector& v) const { return Quantity(x * v.x + y * v.y + z * v.z, unit * v.unit); }
 		inline friend Vector operator * (const Quantity& q, const Vector& v) { return Vector(q.quantity * v.x, q.quantity * v.y, q.quantity * v.z, q.unit * v.unit); }
@@ -88,21 +93,59 @@ namespace phys {
 		Vector operator / (double d) const { return Vector(x / d, y / d, z / d, unit); }
 		Vector operator / (const Quantity& q) const { return Vector(x / q.quantity, y / q.quantity, z / q.quantity, unit / q.unit); }
 
-		friend std::ostream& operator << (std::ostream& o, Vector v) { return o << '<' << v.x << ", " << v.y << ", " << v.z << "> " << v.unit; }
+		std::string toString() const { return "<" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z) + "> " + unit.toString(); }
+
+		friend std::ostream& operator << (std::ostream& o, Vector v) { return o << v.toString(); }
 	};
 
+#define MAKE_SPECIALIZED_CLASS(className, superClass, unitName) \
+class className : public superClass {\
+private:\
+	superClass enforce(const superClass& q) {\
+		if (q.unit != unitName) throw Exception("Unit must be " unitName " (was " + q.toString() + ")"); \
+		return q;\
+	}\
+public:\
+	className(superClass q) : superClass(enforce(q)) {}\
+
+
+#define MAKE_SPECIALIZED_QUANTITY(className, unitName) \
+MAKE_SPECIALIZED_CLASS(className, Quantity, unitName) \
+	className(double d) : Quantity(d, unitName) {} \
+}
+
+	MAKE_SPECIALIZED_QUANTITY(Mass, "kg");
+	MAKE_SPECIALIZED_QUANTITY(Charge, "C");
+
+#define MAKE_SPECIALIZED_VECTOR(className, unitName) \
+MAKE_SPECIALIZED_CLASS(className, Vector, unitName) \
+	className(double x = 0.0, double y = 0.0, double z = 0.0) : Vector(x, y, z, unitName) {} \
+};\
+MAKE_SPECIALIZED_QUANTITY(className##M, unitName)
+
+	MAKE_SPECIALIZED_VECTOR(Force, "N");
+	MAKE_SPECIALIZED_VECTOR(Distance, "m");
+	MAKE_SPECIALIZED_VECTOR(Acceleration, "m / s^2");
+	MAKE_SPECIALIZED_VECTOR(Velocity, "m / s");
+
+#undef MAKE_SPECIALIZED_QUANTITY
+#undef MAKE_SPECIALIZED_VECTOR
+#undef MAKE_SPECIALIZED_CLASS
 
 	// Constant Quantities
 	namespace cnstQ {
 		const Quantity epsilon0 = Quantity(8.854187817e-12, "C^2 / N m^2");
 		const Quantity K = 1 / (4 * M_PI * epsilon0);
-		const Quantity e = Quantity(1.602e-19, "C");
 		const Quantity G = Quantity(6.67e-11, "m^3 / s^2 kg");
+
+		const Charge e = Quantity(1.602e-19, "C");
+		const Mass electronMass = Quantity(9.11e-31, "kg");
 	}
 
-	inline Vector elecForce(Quantity e1, Quantity e2, Vector distance) { return cnstQ::K * e1 * e2 * distance.unitV() / (distance * distance); }
-	inline Quantity elecForce(Quantity e1, Quantity e2, Quantity distance) { return (cnstQ::K * e1 * e2 / (distance * distance)).magnitude(); }
+	Force elecForce(const Charge&, const Charge&, const Distance&);
+	ForceM elecForce(const Charge&, const Charge&, const DistanceM&);
 
-	inline Vector gravForce(Quantity m1, Quantity m2, Vector distance) { return cnstQ::G * m1 * m2 / (distance * distance) * distance.unitV(); }
-	inline Quantity gravForce(Quantity m1, Quantity m2, Quantity distance) { return cnstQ::G * m1 * m2 / (distance * distance); }
+	Force gravForce(const Mass&, const Mass&, const Distance&);
+	ForceM gravForce(const Mass&, const Mass&, const DistanceM&);
+	VelocityM circularVelocity(const DistanceM& r, const AccelerationM& a);
 }
